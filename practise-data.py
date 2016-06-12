5dcabdc0fa04e5d12f9107e8239f0c859aa205ac
26huitailang <26huitailang@gmail.com>
2016-06-12 18:09:32 +0800

    Merge branch 'local-test' into local-test-practice

---
+++ practise-data.py
@@ -0,0 +1,5 @@
+names = np.array(['Bob', 'Joe', 'Will', 'Joe', 'Joe'])
+
+data = {'state': ['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada'],
+        'year': [2000, 2001, 2002, 2001, 2002],
+        'pop': [1.5, 1.7, 3.6, 2.4, 2.9]}
